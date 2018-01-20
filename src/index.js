499fe9b388046ffd5b7b9c29bbc8f52031d1dba3
Trireir <adriblabla@gmail.com>
2018-01-20 21:34:08 +0100

    Added /getUserInfo

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const Axios = axios.create({
 
 const app = express()
 
-app.get('/api/getUserId?:username', (req, res) => {
+app.get('/api/getUserId', (req, res) => {
   if(!req.query.username) {
     return res.status(200).json({});
   }
@@ -20,6 +20,17 @@ app.get('/api/getUserId?:username', (req, res) => {
     if(result.data.response.success !== 1) {
       return res.status(404).json(result.data.response);
     }
+    res.status(200).json(result.data.response);
+  });
+});
+
+app.get('/api/getUserInfo', (req, res) => {
+  if(!req.query.userid) {
+    return res.status(200).json({});
+  }
+  const userIds = req.query.userid.split(',')
+  Axios.get(pathnames.getUserInfo(userIds))
+  .then((result) => {
     res.status(200).json(result.data.response);
   });
 });
--- src/pathnames.js
+++ src/pathnames.js
@@ -1,7 +1,10 @@
 const STEAM_KEY = process.env.STEAM_KEY;
 
 const pathnames = {
-  getUserId: (username) => (`/ResolveVanityURL/v0001/?key=${STEAM_KEY}&vanityurl=${username}`)
+  getUserId: (username) => (`/ResolveVanityURL/v0001/?key=${STEAM_KEY}&vanityurl=${username}`),
+  getUserInfo: (userids) => (
+    `http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${STEAM_KEY}&steamids=[${userids.join(',')}]`
+  ),
 }
 
 export default pathnames;
